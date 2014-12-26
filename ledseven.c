2f9e86a6c67da2ecbfcd7078847c3bbb9a25aee5
Ethan Ruffing <ruffinge@gmail.com>
2014-12-26 17:12:21 -0500

    Added license info to source files

--- ledseven.c
+++ ledseven.c
@@ -9,6 +9,24 @@
  * @author Ethan Ruffing <ruffinge@gmail.com>
  * @since 2014-12-25
  * @version 0.2.0
+ *
+ * @copyright Copyright 2014 by Ethan Ruffing <ruffinge@gmail.com>
+ *
+ * @license
+ * This file is part of ledseven.
+ * @par
+ * Ledseven is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * @par
+ * Ledseven is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * @par
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ledseven.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "ledseven.h"
